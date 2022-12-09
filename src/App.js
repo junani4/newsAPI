bcec08edb6d1cc36f2da200db3a259dd27793f8c
junani4 <namsan7777@naver.com>
2022-12-09 22:39:19 +0900

    뉴스 API

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import { useState, useCallback } from 'react';
+import NewsList from './components/NewsList';
+import Categories from './components/Categories';
+
+const App = () => {
+  const [category, setCategory] = useState('all');
+  const onSelect = useCallback((category) => setCategory(category), []);
+  return (
+    <div>
+      <Categories category={category} onSelect={onSelect} />
+      <NewsList category={category} />;
+    </div>
+  );
+};
+
+export default App;
